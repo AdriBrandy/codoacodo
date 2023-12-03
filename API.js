c14cf0a2de25dbd809e94ad8b251ccf7768a82b2
Adriana Brandan <145227272+AdriBrandy@users.noreply.github.com>
2023-12-03 19:35:37 -0300

    arreglos styles, responsive

--- API.js
+++ API.js
@@ -1,14 +1,16 @@
 /* Se utiliza la API de cotización del bitcoin para simular un cotizador en moneda ficticia de Hrary Potter */
 
 function traer() {
+
     fetch ('https://api.coindesk.com/v1/bpi/currentprice.json')
     .then (res => res.json()
         ) 
     .then (data=> {
-       usd = parseInt(data.bpi.USD.rate)*7.35
-       pound = parseInt(data.bpi.GBP.rate)*4.93
-       euro = parseInt(data.bpi.EUR.rate)*5.9
+       usd = (parseInt(data.bpi.USD.rate)*7.35).toFixed(2)
+       pound = (parseInt(data.bpi.GBP.rate)*4.93).toFixed(2)
+       euro = (parseInt(data.bpi.EUR.rate)*5.9).toFixed(2)
     document.getElementById("cotizacion").innerHTML=(`USD: ${usd}` +"<br>" + `Euro: ${euro}` +"<br>" + `Libras Esterlinas: ${pound}`)
     }
     )
 }
+
